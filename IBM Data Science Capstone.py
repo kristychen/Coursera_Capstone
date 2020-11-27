81b46980287d9b63da42f885537f3e18b3cb0b98
kristychen <67177801+kristychen@users.noreply.github.com>
2020-11-27 04:45:26 -0500

    Add files via upload

---
+++ IBM Data Science Capstone.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# This project is used for Coursera Capstone, it's called "Neighbourhood Battle", it decides similar neighbourhoods using location data.
+
+# In[7]:
+
+
+import pandas as pd
+import numpy as np
+
+print('Hello Capstone Project Course!')
+
